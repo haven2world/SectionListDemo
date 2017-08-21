395ae67b9fc43a125cb945b7023c61312760ddf0
chenhaowen <chenhaowen@kingsoft.com>
2017-08-21 14:44:36 +0800

    [update] prop-types

--- SiderSectionList.js
+++ SiderSectionList.js
@@ -1,4 +1,5 @@
-import React, {PureComponent, PropTypes} from 'react'
+import React, {PureComponent} from 'react'
+import PropTypes from 'propTypes';
 import {Text, View, StyleSheet} from 'react-native'
 const returnTrue = () => true;
 
