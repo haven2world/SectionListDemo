18cd4a801c950d6343d7debe277f08fabc77b720
chenhaowen <chenhaowen@kingsoft.com>
2017-08-21 14:06:36 +0800

    [update] test

--- index.android.js
+++
@@ -1,8 +0,0 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-import {AppRegistry,} from 'react-native';
-import Main from './app/Main'
-AppRegistry.registerComponent('SectionListDemo', () => Main);
